23bdd5214fc4f4a733ee9c593f49790c459952a4
NewProjects <newprojects@manishs-50g-mbp.lan>
2018-05-05 10:34:43 -0700

    Orion rises in the east

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Copyright (C) 2018, Manish Vaidya -->
+
+<!doctype html>
+ 
+<?php include 'php_includes/header.php'; ?>
+
+<?php include 'php_includes/navbar.php'; ?>
+
+<!-- Primary Page Layout -->
+
+
+<?php include 'php_includes/footer.php'; ?>
+
+
+
+</body>
+</html>
---
+++ php_includes/footer.php
@@ -0,0 +1,16 @@
+<!-- Copyright (C) 2018, Manish Vaidya -->
+
+<footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        </div>
+</footer>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script>
+ 
