c1db6923e79864e323c6b92dfabdfca338c03b6b
ManishV <manishnewprojects@gmail.com>
2018-07-26 23:04:44 -0700

    working startup, local dest, all search

--- index.php
+++ index.php
@@ -29,19 +29,33 @@
 
 <!-- Primary Page Layout -->
 
-<body>
 
  
 <?php include 'php_includes/navbar.php'; ?>
 
+<body>
 
- 
+    <!-- WORKS
     <div id="map"></div>
-    
    <script> getLocation(); </script>
+   -->
+ <body style="margin:0px; padding:0px;" onload="initMap()">
+  
+    <div>
+         <label for="raddressInput">Search location:</label>
+         <input type="text" id="addressInput" size="15"/>
+        <label for="radiusSelect">Radius:</label>
+        <select id="radiusSelect" label="Radius">
+          <option value="50" selected>50 kms</option>
+          <option value="30">30 kms</option>
+          <option value="20">20 kms</option>
+          <option value="10">10 kms</option>
+        </select>
 
-   
-
+        <input type="button" id="searchButton" value="Search"/>
+    </div>
+    <div><select id="locationSelect" style="width: 10%; visibility: hidden"></select></div>
+    <div id="map" style="width: 100%; height: 90%"></div>
     
 
 <?php include 'php_includes/footer.php'; ?>
--- php_includes/header.php
+++ php_includes/header.php
@@ -14,23 +14,24 @@
 
  <!-- Bootstrap core CSS -->
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 
 
 <!-- CSS Files -->
+<link rel="stylesheet" type="text/css" href="css/jquery.spin.css">
 <link rel="stylesheet" type="text/css" href="css/mv_css_defines.css">
 
-
-<!-- Website Fonts & icons -->
-   
+ 
 <!-- JQuery-->
 <script charset="utf-8" type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  
-<!-- Local JS --> 
 
 <!-- Local JS --> 
-<script charset="utf-8" type="text/javascript" src="js/orion.js"></script>
+<script charset="utf-8" type="text/javascript" src="js/jquery.spin.js"></script>
+<script charset="utf-8" type="text/javascript" src="js/orion_mapping_functions.js"></script>
+<script charset="utf-8" type="text/javascript" src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.5/dist/loadingoverlay.min.js"></script>
+
    
  
 <!-- Google Maps API    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCyzNh1jUjz9QJ9VE_GO0DBV8wGM6li_EU&libraries=places"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2-_L2ge3x5vEG6h_PSRWQO9daQ9PgB4k&libraries=places"></script>
